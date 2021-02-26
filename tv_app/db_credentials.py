a737ddb79ec3a3ee0e8147fc490b232c428addc4
jooanc <jooanchoi@protonmail.com>
2021-02-25 19:10:16 -0800

    added db

--- tv_app/db_credentials.py
+++ tv_app/db_credentials.py
@@ -1,11 +1,17 @@
 #jooan db
-host = 'classmysql.engr.oregonstate.edu'
-user = 'cs340_choijoo'
-password = '4081'
-db = 'cs340_choijoo'
+#host = 'classmysql.engr.oregonstate.edu'
+#user = 'cs340_choijoo'
+#password = '4081'
+#db = 'cs340_choijoo'
 
 #brandi db
 #host = 'classmysql.engr.oregonstate.edu'
 #user = 'cs340_kuritzb'
 #password = '6188'
 #db = 'cs340_kuritzb'
+
+#new db
+host = 'sql3.freemysqlhosting.net'
+user = 'sql3395247'
+password = 'lUhngtURl1'
+db = 'sql3395247'
